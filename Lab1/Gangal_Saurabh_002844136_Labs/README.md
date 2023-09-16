a90479730d4bfd2fecc94aade7ef393219ae5e9f
saurabhgangal <gangal.s@northeastern.edu>
2023-09-17 03:30:22 +0530

    Add files via upload

---
+++ Lab1/Gangal_Saurabh_002844136_Labs/README.md
@@ -0,0 +1,2 @@
+# Gangal_Saurabh_002844136_Labs
+Lab Work for Application Engineering and Development (INFO5100 12111)
---
+++ Lab1/src/model/Product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author saurabhgangal
+ */
+public class Product {
+    
+   private String name;
+   private String desc;
+   private String avalnum;
+   private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAvalnum() {
+        return avalnum;
+    }
+
+    public void setAvalnum(String avalnum) {
+        this.avalnum = avalnum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    
+}
